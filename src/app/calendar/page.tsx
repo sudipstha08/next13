64af0dbb282895a254c74df5f0e7a74728621673
sudipstha08 <sudipstha08@gmail.com>
2024-02-18 13:21:07 +0545

    feat: add calendar

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+//https://www.npmjs.com/package/@fullcalendar/react
+// https://www.npmjs.com/package/@devexpress/dx-react-scheduler?activeTab=readme
+const page = () => {
+  return <div>page</div>
+}
+
+export default page
+
+//https://www.npmjs.com/package/react-big-calendar
